3eea7995ac46e94743904145274acce9043c3504
4072 <hariyanijenish@gmail.com>
2023-11-06 19:09:54 +0530

    thered

---
+++ MARKSHIT.C
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+int f,s,t,fo,fi,si,se,e, sum;
+clrscr();
+printf("Enter sanskrit");
+scanf("%d",&f);
+printf("Enter drawing");
+scanf("%d",&s);
+printf("Enter Gujrati");
+scanf("%d",&t);
+printf("Enter saince");
+scanf("%d",&fo);
+printf("Enter so. saince");
+scanf("%d",&fi);
+printf("Enter maths");
+scanf("%d",&si);
+printf("Enter english");
+scanf("%d",&se);
+printf("Enter hindi");
+scanf("%d",&e);
+sum=f+s+t+fo+fi+si+se+e;1
+printf("Sum is ==%d\n",sum);
+printf("Avreg is ==%d",sum*100/800);
+getch();
+}
